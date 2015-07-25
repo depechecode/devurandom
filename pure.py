00ae04c62109e3e058bcca9e0bef0ba6d0155652
bergenb <ciaranfitzpatrick@Ciarans-MacBook-Pro.local>
2015-07-25 18:55:23 +0100

    Java exec added

--- pure.py
+++ pure.py
@@ -1,14 +1,29 @@
 import os
 #txt = open('file.php', 'r')
 
-for root, dirs, files in os.walk("/Users/Documents"):
+def phpFind(file):
+	print file
+	txt = open(file, 'r')
+	for line in txt:
+		if "eval" in line and "$_" in line:
+			print "Code Exec Found!?"
+			print line
+		else:
+			print "."
+
+def javaFind(file):
+	print file
+	txt = open(file, 'r')
+	for line in txt:
+		if ".exec" in line:
+			print "Code Exec Found!?"
+			print line
+		else:
+			print "."
+
+for root, dirs, files in os.walk("/Users/ciaranfitzpatrick/Documents"):
 	for file in files:
 		if file.endswith(".php"):
-			print file
-			txt = open(file, 'r')
-			for line in txt:
-				if "eval" in line and "$_" in line:
-					print "Code Exec Found?"
-					print line
-				else:
-					print "Nothing!"
+			phpFind(file)
+		elif file.endswith(".java"):
+			javaFind(file)
