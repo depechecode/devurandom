42667d63396f154520f176cdac7671f484fa995c
bergenb <ciaranfitzpatrick@Ciarans-MacBook-Pro.local>
2015-08-08 22:02:35 +0100

    More concise

--- upscala.py
+++ upscala.py
@@ -1,27 +1,24 @@
 import os
 
-def sqlInj(file):
+def grepForBug(file):
 	print file
 	txt = open(file, 'r')
 	for line in txt:
 		if("Query") in line and " + " in line:
 			print "Possible Dynamic SQL Statement Found"
 			print line
-		else:
-			print "."
-
-def comExec(file):
-	print file
-	txt = open(file, 'r')
-	for line in txt:
-		if ("sys.process") in line:
+		elif("sys.process") in line:
 			print "Review file for OS Interaction"
+			print line
+		elif("XMLDecoder(") in line:
+			print "Review file XXE Issues"
 			print line
 		else:
 			print "."
 
-for root, dirs, files in os.walk("/Users/Documents"):
+for root, dirs, files in os.walk("/Users/ciaranfitzpatrick/Documents"):
 	for file in files:
 		if file.endswith(".scala"):
-			sqlInj(file)
-			comExec(file)
+			grepForBug(file)
+		else:
+			print "No scala files found in folder..."
