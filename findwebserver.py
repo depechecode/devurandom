8a9a85689a053f5a060869af48928170f5f79b9e
bergenb <ciaranfitzpatrick@Ciarans-MacBook-Pro.local>
2015-09-05 12:30:35 +0100

    Trim it back

--- findwebserver.py
+++ findwebserver.py
@@ -1,15 +1,8 @@
-import urllib2
 from netaddr import IPNetwork
 
 subnet = raw_input("Please enter target subnet: ")
 
 for ip in IPNetwork(subnet).iter_hosts():
-    #print '%s' % ip
+	print "http://" + "%s" % ip
     #target = "http://" + ip + "/script"
-    target = "http://" + str(ip) + "/"
-    print "Trying " + target
-    try:
-        response = urllib2.urlopen(target)
-        print response
-    except urllib2.HTTP.Error, e:
-        print "No Server Found at " + target
+    
