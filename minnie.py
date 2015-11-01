355025ed6d362f6a5ce3ef843407d9acca56291e
bergenb <ciaranfitzpatrick@Ciarans-MacBook-Pro.local>
2015-11-01 13:01:03 +0000

    Find api calls in minified js

---
+++ minnie.py
@@ -0,0 +1,12 @@
+import bs4
+import requests
+
+url = raw_input("Enter a website to extract the URL's from: ")
+
+page = requests.get(url)
+page.raise_for_status()
+
+soup = bs4.BeautifulSoup(page.text)
+
+for link in soup.find_all('script'):      #Lists out css links
+    print(link.get('src'))
