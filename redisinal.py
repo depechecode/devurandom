57fe5365b5bd6eacd0d54d1795da2c4941b3e83b
bergenb <ciaranfitzpatrick@Ciarans-MacBook-Pro.local>
2015-11-07 00:06:06 +0000

    Redis Poking

---
+++ redisinal.py
@@ -0,0 +1,30 @@
+import socket
+import telnetlib
+from netaddr import IPNetwork
+
+subnet = raw_input("Please enter target subnet: ")
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+for ip in IPNetwork(subnet).iter_hosts():
+	result = sock.connect_ex((str(ip),6379))
+	if result == 0:
+   		print "Port is open on %s" % str(ip)
+   		print ""
+   		print "Connecting to Redis: "
+   		tn.write("CONFIG GET *"+"\n")
+   		output = tn.read_all()
+   		if "dbfilename" in output:
+   			print "Looks like you can manipulate the CONFIG.."
+   		else:
+   			print "Not sure this redis is vulnerable.."
+	else:
+   		print "Port is not open %s" % str(ip)
+
+
+
+
+
+
+
+
