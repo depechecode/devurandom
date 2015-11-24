d1c76287cc8ab21ad7fc790b686849d57a38f86f
Robert Fitzpatrick <robertfitzpatrick1@gmail.com>
2015-11-24 09:53:22 +0000

    Delete findwebserver.py

--- findwebserver.py
+++
@@ -1,8 +0,0 @@
-from netaddr import IPNetwork
-
-subnet = raw_input("Please enter target subnet: ")
-
-for ip in IPNetwork(subnet).iter_hosts():
-	print "http://" + "%s" % ip
-    #target = "http://" + ip + "/script"
-    
